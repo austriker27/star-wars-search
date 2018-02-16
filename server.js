dce855bf8671334a89985a68cb89b42b016e4e56
David Lindahl <davidalindahl@gmail.com>
2018-02-16 15:09:20 -0700

    added request npm library and basic request to the api

--- server.js
+++ server.js
@@ -4,17 +4,42 @@ const EXPRESS = require('express');
 const PARSER = require('body-parser');
 
 const APP = EXPRESS();
-const API_URL = (`http://swapi.co/api/people/`)
 const PORT = process.env.PORT || 3000;
+const request = require('request');
 
 APP.use(PARSER.json());
 APP.use(PARSER.urlencoded({ extended: true }));
 APP.use(EXPRESS.static('./public'));
 
-APP.get('*', function(request, response){
-  response.sendFile('index.html', {root: './public'})
+const API_URL = (`http://swapi.co/api/people/`)
+
+
+APP.get('API/search', searchCharacters);
+
+let searchCharacters = (request, response) => {
+}
+
+let options = { 
+  method: 'GET',
+  url: API_URL,
+  qs: { 
+    search: '${searchString}'
+  },
+  };
+
+request(options, (error, response, body) => {
+  if (error) throw new Error(error);
+
+  console.log('body:', body);
 });
 
-APP.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`)
-});
+
+
+
+// APP.get('*', (request, response) => {
+//   response.sendFile('index.html', {root: './public'})
+// });
+
+// APP.listen(PORT, () => {
+//   console.log(`listening on port ${PORT}`)
+// });
