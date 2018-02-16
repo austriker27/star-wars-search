00ca5c271fcfa58ad180a99dc408a88b52b03b1f
David Lindahl <davidalindahl@gmail.com>
2018-02-16 13:55:17 -0700

    setting up the server for basic operation

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const EXPRESS = require('express');
+const PARSER = require('body-parser');
+
+const APP = EXPRESS();
+const API_URL = (`http://swapi.co/api/people/`)
+const PORT = process.env.PORT || 3000;
+
+APP.use(PARSER.json());
+APP.use(PARSER.urlencoded({ extended: true }));
+APP.use(EXPRESS.static('./public'));
+
+APP.get('*', function(request, response){
+  response.sendFile('index.html', {root: './public'})
+});
+
+APP.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`)
+});
