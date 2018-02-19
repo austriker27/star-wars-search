5a9b06307d0194e6bdb57c9807e4d4e426d7957f
David Lindahl <davidalindahl@gmail.com>
2018-02-18 18:10:52 -0800

    added in jquery for simple ajax request

--- public/script.js
+++ public/script.js
@@ -1,2 +1,22 @@
 'use strict';
 
+$.ajax({
+  type: 'GET',
+  url: 'http://swapi.co/api/people/',
+  data: {userId: '1'},
+  beforeSend: () => {
+    // append loading image
+    $('#search-panel').html('<div class="Loading"><img src="/images/loading.gif" alt="loading image" /> </div>');
+  },
+  success: (data) => {
+    // this is what you do with the data
+    $('search-panel').empty();
+    $(data).find('item').each(function(i){
+      $('search-panel').append('<h4>' + $(this).find('name').text() + '</h4><p>' + $(this).find('gender').text() + '</p>');
+    });
+  },
+  error: () => {
+    $('#search-panel').html('<p class="error">Uh oh, no results found. Try that again soon. </p>');
+
+  }
+})
