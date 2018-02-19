b98c5e4853a187bdc4431245499ff75346bfcaad
David Lindahl <davidalindahl@gmail.com>
2018-02-19 14:02:26 -0800

    got script file working to pull back data from SWAPI using hardcoded name

--- public/script.js
+++ public/script.js
@@ -1,26 +1,24 @@
 'use strict';
 
 $.ajax({
+  url: 'https://swapi.co/api/people/1',
+  // data: {userId: '1'},
   type: 'GET',
-  url: 'https://swapi.co/api/people/',
-  data: {userId: '1'},
-
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-  },
+  crossDomain: true,
+  dataType: 'json',
 
   beforeSend: () => {
     // append loading image
     $('#search-form').html('<div class="Loading"><img src="/images/loading.gif" alt="loading image" /> </div>');
   },
 
-  success: (data) => {
+  success: (response) => {
     // this is what you do with the data
-    $('search-form').empty();
-    $(data).find('item').each(function(i){
-      $('search-form').append('<h4>' + $(this).find('name').text() + '</h4><p>' + $(this).find('gender').text() + '</p>');
-    });
+    $('#search-form').empty();
+    console.log(response);
+    $('.characterTable').text(response.name);
   },
+
   error: () => {
     $('#search-form').html('<p class="error">Uh oh, no results found. Try that again soon. </p>');
 
